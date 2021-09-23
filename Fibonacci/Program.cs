7447c7c48a8d6606e94ebc97b910d91f177b0088
Mauricio Montealegre <mmontealegre@abilis-solutions.com>
2021-09-22 22:29:56 -0400

    Fibonnaci examples dynamic programming

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var result = Fibonacci(30);
+            stopWatch.Stop();
+            Console.WriteLine("Fibonacci 30=" + result + " RunTime Fibonacci: " + stopWatch.ElapsedTicks);
+
+            Stopwatch stopWatch2 = new Stopwatch();
+            stopWatch2.Start();
+            result = FibonacciMemo(30);
+            stopWatch2.Stop();
+            Console.WriteLine("Fibonacci 30=" + result + " RunTime FibonacciMemo: " + stopWatch2.ElapsedTicks);
+
+            Stopwatch stopWatch3 = new Stopwatch();
+            stopWatch3.Start();
+            result = FibonacciBottomUp(30);
+            stopWatch3.Stop();
+            Console.WriteLine("Fibonacci 30=" + result + " RunTime FibonacciMemo: " + stopWatch3.ElapsedTicks);
+            Console.ReadLine();
+
+        }
+
+
+
+        /// <summary>
+        /// Recursive Fibonacci
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        static long Fibonacci(int n)
+        {
+            if (n == 0) return 1;
+            if (n == 1) return 1;
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        private static readonly Dictionary<int, long> cache = new Dictionary<int, long>() { { 0, 1 }, { 1, 1 } };
+
+        /// <summary>
+        /// Fibonacci with Memoization
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        static long FibonacciMemo(int n)
+        {
+            // These values are now in the cache
+            //if (n == 0) return 1;
+            //if (n == 1) return 1;
+
+            if (cache.ContainsKey(n))
+                return cache[n];
+
+
+            var result = FibonacciMemo(n - 1) + FibonacciMemo(n - 2);
+            cache[n] = result;
+            return result;
+        }
+
+        /// <summary>
+        /// Fibonacci BottomUp approach
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        static long FibonacciBottomUp(int n)
+        {
+            var a = 1; // fib(n-2)
+            var b = 1; // fib(n-1)
+            for (int i = 2; i < n + 1; i++)
+            {
+                var temp = a;
+                a = b;
+                b = temp + b;
+            }
+
+            return b;
+        }
+    }
+}
